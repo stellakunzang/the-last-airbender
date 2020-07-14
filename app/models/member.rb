88b1afd6579bcdff6fe029398ad5a9b0a7b5fa46
stellakunzang <stella.k.bonnie@gmail.com>
2020-07-14 10:43:18 -0600

    Create member objects

---
+++ app/models/member.rb
@@ -0,0 +1,14 @@
+class Member
+
+  attr_reader :name,
+              :allies,
+              :enemies,
+              :photo_url
+
+  def initialize(data)
+    @name = data[:name]
+    @allies = data[:allies]
+    @enemies = data[:enemies]
+    @photo_url = data[:photoUrl]
+  end
+end
--- app/poros/search_result.rb
+++ app/poros/search_result.rb
@@ -1,7 +1,26 @@
 class SearchResult
-  def initialize(nation)
+
+  attr_reader :name,
+              :nation
+
+  def initialize(nation_name)
+    @name = format_name(nation_name)
+    @nation = format_nation(nation_name)
+  end
+
+  def format_name(nation_name)
+    nation_name.split("_")[1]
+  end
+
+  def format_nation(nation_name)
+    nation_name.split("_")[0]
   end
 
   def members
-  end 
+    last_airbender_service = LastAirbenderService.new
+    members = last_airbender_service.members(@nation, @name)
+    members.map do |member_data|
+      Member.new(member_data)
+    end
+  end
 end
