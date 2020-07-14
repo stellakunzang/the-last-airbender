c0f6e3ed921d96a6768605783ebaf792a13acfa7
stellakunzang <stella.k.bonnie@gmail.com>
2020-07-14 09:18:56 -0600

    Fix test

--- spec/features/visitor_searches_for_members_spec.rb
+++ spec/features/visitor_searches_for_members_spec.rb
@@ -1,22 +1,23 @@
 require 'rails_helper'
 
-feature 'visitor searches for members' do
-  visit "/"
+describe 'visitor searches for members' do
+  it "can see members of a nation" do
+    visit "/"
 
-  select "Fire Nation", from: "element nation"
+    select "Fire Nation", from: "nation"
 
-  click_on "Search For Members"
+    click_on "Search For Members"
 
-  expect(current_path).to eq("/search")
+    expect(current_path).to eq("/search")
 
-  expect(page).to have_content("20 Members")
+    expect(page).to have_content("20 Members")
 
-  expect(page).to have_css(".member", count: 20)
+    expect(page).to have_css(".member", count: 20)
 
-  within(first(".member")) do
-    expect(page).to have_css(".name")
-    expect(page).to have_css(".allies")
-    expect(page).to have_css(".enemies")
+    within(first(".member")) do
+      expect(page).to have_css(".name")
+      expect(page).to have_css(".allies")
+      expect(page).to have_css(".enemies")
+    end
   end
-  
 end
